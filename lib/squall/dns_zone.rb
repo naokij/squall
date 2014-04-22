eea9a6ef735a79211129a272bd89f8fa1e7403cb
Suhail Patel <me@suhailpatel.com>
2014-04-22 14:36:43 +0100

    Add methods for creating, editing and deleting zone records

--- lib/squall/dns_zone.rb
+++ lib/squall/dns_zone.rb
@@ -50,6 +50,39 @@ module Squall
       response['dns_zone']
     end
 
+    # Public: Adds new DNS Zone Record for a DNS Zone.
+    #
+    # zone_id - ID of the DNS zone
+    # options - dns_record hash
+    #
+    # Returns a DNS Record hash.
+    def create_record(zone_id, options)
+      response = request(:post, "/dns_zones/#{zone_id}/records.json", options)
+      response['dns_record']
+    end
+
+    # Public: Edits a DNS Zone Record for a DNS Zone.
+    #
+    # zone_id - ID of the DNS zone
+    # record_id - ID of the Record
+    # options - dns_record hash
+    #
+    # Returns a DNS Record hash.
+    def edit_record(zone_id, record_id, options)
+      response = request(:put, "/dns_zones/#{zone_id}/records/#{record_id}.json", options)
+      response['dns_record']
+    end
+
+    # Public: Deletes a DNS Zone Record in a DNS Zone.
+    #
+    # zone_id - ID of the DNS zone
+    # record_id - ID of the Record
+    #
+    # Returns an empty hash.
+    def delete_record(zone_id, record_id)
+      request(:delete, "/dns_zones/#{zone_id}/records/#{record_id}.json")
+    end
+
     # Public: Grabs the configured nameservers.
     #
     # Returns an array of records.
